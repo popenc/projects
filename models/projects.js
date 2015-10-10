67267e89a20febd5a810f533f320699e0b15c12e
popenc <i.nickpope@gmail.com>
2015-10-10 10:35:33 -0400

    samesies

---
+++ models/projects.js
@@ -0,0 +1,7 @@
+var tempeh = {
+	'proj_name': 'tempeh',
+	'links': ['#new-batch', '#realtime-data', '#old-batches', '#tempeh-tales'],
+	'labels': ['new batch', 'realtime data', 'old batches', 'tempeh tales']
+};
+
+module.exports = tempeh;
