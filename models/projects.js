6fb8029afb2325f203189bf4cbf5018a76a59be2
popenc <i.nickpope@gmail.com>
2015-10-10 12:22:10 -0400

    adding the blog stuff, mostly for tempeh at the moment, then a generalized thing for any project.

--- models/projects.js
+++ models/projects.js
@@ -1,5 +1,13 @@
+//- This should ultimately be a part of the DB and not just a static object
+
 var tempeh = {
 	'proj_name': 'tempeh',
+
+	'blog_desc': "A journey into tempeh cultivation",
+	'post_title': "First post for tempeh!",
+	'post_date': "10 October 2015",
+	'post_content': "",
+	
 	'links': ['#new-batch', '#realtime-data', '#old-batches', '#tempeh-tales'],
 	'labels': ['new batch', 'realtime data', 'old batches', 'tempeh tales']
 };
