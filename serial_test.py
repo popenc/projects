21847b281436062c1b61834d33057454040c36fa
popenc <i.nickpope@gmail.com>
2016-09-10 19:28:09 -0400

    here it is

--- serial_test.py
+++ serial_test.py
@@ -1,8 +1,18 @@
 import serial
+import time
 
-arduino = serial.Serial("COM4", 9600, timeout=.1)
+arduino = serial.Serial("COM5", 9600, timeout=.1)
+data_file = open('temp_data_fridge.txt', 'a')
+
 while True:
-	data = arduino.readline()[:-2]  # last bit gets rid of newline chars
+	# data = arduino.readline()[:-2]  # last bit gets rid of newline chars
+	data = arduino.readline()
+	the_time = time.strftime('%Y-%m-%dT%H:%M:%S')
+
 	if data:
-		print data
-		# 
+		
+		result = the_time + ', ' + data + '\n'
+		
+		with open('temp_data_fridge.txt', 'a') as data_file:
+			print result
+			data_file.write(result) 
